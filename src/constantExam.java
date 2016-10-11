f81d0d709f3be943516a6b47218349ea92959b55
josung50 <josung50@naver.com>
2016-10-11 15:22:42 +0900

    Java Project(BinarySearch , BubbleSort , HelloWorld)

---
+++ src/constantExam.java
@@ -0,0 +1,17 @@
+
+public class constantExam {
+
+	public static void main(String[] args) {
+		int i;
+		i = 10;
+		i = 5 ;
+		
+		final int J;
+		J = 10;
+		double circleArea;
+		final double PI = 3.1459;
+		circleArea = 3*3*PI;
+		System.out.println(circleArea);
+	}
+
+}
